dc75001c7dea2280228763e9088bb48595a11632
Szczepanowska <szczepanowska.n@gmail.com>
2019-06-15 12:12:54 +0200

    First Commit

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,21 @@
+
 package com.kodilla.testing;
+
+import com.kodilla.testing.calculator.Calculator;
 
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+
+
+
+        Calculator Calculator = new Calculator();
+        int result2 = Calculator.subtractAFromB(10,3);
+        int result = Calculator.addAToB(5, 5);
+
+
+
+        System.out.println(result2);
+        System.out.println(result);
+
     }
 }
