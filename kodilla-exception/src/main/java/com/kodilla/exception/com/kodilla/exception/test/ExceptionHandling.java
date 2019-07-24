c6f357830a9eacb7c2a1d313cbff6fc6be704d99
Szczepanowska <szczepanowska.n@gmail.com>
2019-07-24 18:26:42 +0200

    Commit

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/com/kodilla/exception/test/ExceptionHandling.java
@@ -0,0 +1,16 @@
+package com.kodilla.exception.com.kodilla.exception.test;
+
+public class ExceptionHandling {
+public static void main (String args[]){
+    SecondChallenge secondChallenge = new SecondChallenge();
+    try{
+        secondChallenge.probablyIWillThrowException(2.1, 3.5);
+    } catch (Exception e){
+        System.out.println("Problem with numbers " + e);
+
+    } finally {
+        System.out.println("End of challenge");
+    }
+}
+
+}
