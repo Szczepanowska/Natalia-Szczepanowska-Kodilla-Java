09b78101086120eab4ef56ee688d61cea6dadd58
Szczepanowska <szczepanowska.n@gmail.com>
2019-10-07 08:19:16 +0200

    commit

---
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/invoice/dao/InvoiceDaoTestSuite.java
@@ -0,0 +1,48 @@
+package com.kodilla.hibernate.invoice.dao;
+
+import com.kodilla.hibernate.invoice.Invoice;
+import com.kodilla.hibernate.invoice.Item;
+import com.kodilla.hibernate.invoice.Product;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.math.BigDecimal;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InvoiceDaoTestSuite {
+
+    @Autowired InvoiceDao invoiceDao;
+
+    @Test
+    public void testInvoiceDaoSave(){
+        //Given
+        Product product = new Product("printer");
+
+        Item item = new Item(new BigDecimal(1000), 5, new BigDecimal(5000));
+        Item item2 = new Item(new BigDecimal(1000), 50, new BigDecimal(50000));
+
+        Invoice invoice = new Invoice("7");
+        product.getItems().add(item);
+        product.getItems().add(item2);
+        item.setProduct(product);
+        item2.setProduct(product);
+        item.setInvoice(invoice);
+        item2.setInvoice(invoice);
+
+        //When
+        invoiceDao.save(invoice);
+        int id = invoice.getId();
+
+        //Then
+        Assert.assertNotEquals(0, id);
+
+        //CleanUp
+        invoiceDao.deleteById(id);
+
+    }
+}
