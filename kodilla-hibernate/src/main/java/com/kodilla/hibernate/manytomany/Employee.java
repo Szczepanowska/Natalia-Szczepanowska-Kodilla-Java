5f083336039ff090501d59e86c8951d7caa4e8f2
Szczepanowska <szczepanowska.n@gmail.com>
2019-10-10 07:05:09 +0200

    commit

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/manytomany/Employee.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/manytomany/Employee.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 @NamedQuery(
         name = "Employee.retrieveEmployeesGivenLastname",
-        query = "FROM Employee WHERE lastname > :LASTNAME"
+        query = "FROM Employee WHERE lastname = :LASTNAME"
 )
 
 @Entity
